7a0cced1fc033d203da4f51e59e9f417fd63e41e
tranBaoNgocj <tranbaongoc038@gmail.com>
2023-11-24 16:06:39 +0700

    connect and statistic income

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "demo",
         "USER": "root",
-        "PASSWORD": "nguyenhuonggiang",
+        "PASSWORD": "Ghik9536@th",
         "HOST": "localhost",
         "PORT": "3306",
     }
